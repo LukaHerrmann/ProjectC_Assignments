2045bd253017042ae6e815800b1b9fab2d8e0ced
LukaHerrmann <lukajherrmann@gmail.com>
2021-02-10 19:34:54 +0100

    Functies toegevoegd die de kleuren kan omzetten in een cijfercode en andersom

--- Mastermind/CodeMaster_Player.py
+++ Mastermind/CodeMaster_Player.py
@@ -1,1 +1,12 @@
 import random
+
+
+def colortonumb(colors, allcolors):
+    result = ''
+    for color in colors:
+        result += str(allcolors.index(color))
+    return result
+
+
+def numbtocolor(number, allcolors):
+    return [allcolors[int(x)] for x in number]
